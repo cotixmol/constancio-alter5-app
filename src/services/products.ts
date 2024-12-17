4b429d462b6640607f67609a3c10db088bcafbec
Constancio Molinengo <constancio.molinengo@outlook.com>
2024-12-17 17:18:56 -0300

    feat/create getProducts function

---
+++ src/services/products.ts
@@ -0,0 +1,20 @@
+// services/products.ts
+import axios from "axios";
+import { Product } from "@/types/products";
+
+const API_BASE_URL = process.env.API_BASE_URL;
+
+export const getProducts = async (): Promise<{ products: Product[] }> => {
+  try {
+    const response = await axios.get<{ products: Product[] }>(
+      `${API_BASE_URL}/api/products`
+    );
+    const products = response.data.products.map((product) => ({
+      ...product,
+      // TODO: See to modify any of the items that comes from the endpoint or just use some.
+    }));
+    return { products };
+  } catch (error) {
+    throw error;
+  }
+};
