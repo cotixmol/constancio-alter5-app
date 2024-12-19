f31ce71ce38c954df5fd0c415e3ef801b34679bc
Constancio Molinengo <constancio.molinengo@outlook.com>
2024-12-18 22:57:32 -0300

    feat/add first login control in getProduct service

--- src/services/products.ts
+++ src/services/products.ts
@@ -6,8 +6,15 @@ const API_BASE_URL = process.env.API_BASE_URL;
 
 export const getProducts = async (): Promise<{ products: Product[] }> => {
   try {
+    const token = localStorage.getItem("authToken"); // Retrieve the token
+
     const response = await axios.get<{ products: Product[] }>(
-      `${API_BASE_URL}/api/products`
+      `${API_BASE_URL}/api/products`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
     );
     const products = response.data.products.map((product) => ({
       ...product,
